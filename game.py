840677f91e48d04644aaf20c41b2fdb436da024c
plexov01 <69338815+plexov01@users.noreply.github.com>
2020-11-24 17:58:15 +0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+#Задаю ширину и высоту окна
+widthW=360 
+heightW=480
+#Частота кадров в секунду
+FPS=60
+#Создаю игру и окно
+pygame.init()
+screen=pygame.display.set_mode((widthW,heightW)) #Окно игры
+#Название окна
+pygame.display.set_caption("Slime's castle")
+#Clock нужен для того, чтобы убедиться, что игра работает с заданной частотой кадров
+clock=pygame.time.Clock()
+
+#Цикл игры
+running=True
+while running:
+    # держим цикл на правильной скорости
+    clock.tick(FPS)
+    #Цикл для обнаружения нажатых клавиш игроком
+    for event in pygame.event.get():
+        #if для закрытия игры при нажатии на крестик
+        if event.type==pygame.QUIT:
+            running==False
+
+    #flip нужен для однократного показа всего, что нарисовано на экране за 1 кадр
+    pygame.display.flip()
+#При завершении цикла игры окно игры закрывается
+pygame.quit()
