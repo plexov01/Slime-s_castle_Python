7d12a176f1406be9b5366ad92dced8ad1888ac8e
plexov.01 <plexov.01@mail.ru>
2020-11-24 18:35:35 +0700

    main version 0.0.1

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ pygame.init()
 screen=pygame.display.set_mode((widthW,heightW)) #Окно игры
 #Название окна
 pygame.display.set_caption("Slime's castle")
+
+pygame.display.set_icon(pygame.image.load("image\icon\icon_slime.jpg"))
 #Clock нужен для того, чтобы убедиться, что игра работает с заданной частотой кадров
 clock=pygame.time.Clock()
 
@@ -16,14 +18,14 @@ clock=pygame.time.Clock()
 running=True
 while running:
     # держим цикл на правильной скорости
-    clock.tick(FPS)
+    clock.tick(FPS) 
     #Цикл для обнаружения нажатых клавиш игроком
     for event in pygame.event.get():
         #if для закрытия игры при нажатии на крестик
         if event.type==pygame.QUIT:
-            running==False
+            running=False
 
     #flip нужен для однократного показа всего, что нарисовано на экране за 1 кадр
-    pygame.display.flip()
+    pygame.display.update()
 #При завершении цикла игры окно игры закрывается
 pygame.quit()
