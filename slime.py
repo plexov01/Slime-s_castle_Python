09a8c373b590668cc02e8344376ec2762b4a7518
plexov.01 <plexov.01@mail.ru>
2020-12-12 20:58:48 +0700

    version 0.0.5 +Animations slime

--- slime.py
+++ slime.py
@@ -1,16 +1,30 @@
 from pygame import *
+import pyganim
 #Скорость движения слизня
 MoveSpeed=5
 #Сила прыжка
 jumpPower=15
 #Гравитация(потом нужно подогнатьпод реальную)
 gravity=0.3
+#Задержка анимации
+AnimationDelay=0.1
+AnStay=['animation/slime/stay0.png']
 
+#Добавляю анимации
+AnRight =['animation/slime/slimeRight1.png',
+          'animation/slime/slimeRight2.png',
+          'animation/slime/slimeRight3.png',
+          'animation/slime/slimeRight4.png']
+           
+AnLeft = ['animation/slime/slimeLeft1.png',
+          'animation/slime/slimeLeft2.png',
+          'animation/slime/slimeLeft3.png',
+          'animation/slime/slimeLeft4.png']
 
 #Ширина слизня
-widthS=20
+widthS=image.load('animation/slime/stay0.png').get_width()
 #Высота слизня
-heightS=20
+heightS=image.load('animation/slime/stay0.png').get_height()
 #Цвет слизня
 colorS="#3333FF"
 
@@ -28,17 +42,46 @@ class Slime(sprite.Sprite):
         self.onGround=False
         #Как выглядит слизень
         self.image=Surface((widthS,heightS))
-        self.image.fill(Color(colorS))
-        self.rect=Rect(x,y,widthS,heightS) #Прямоугольный объект, для отслеживания местанахождения слизня(нужно заменить на овал)
+        
+        self.rect=self.image.get_rect() #Прямоугольный объект, для отслеживания местанахождения слизня
+        self.rect.x=x
+        self.rect.y=y
+        
+        #Функциядля созданий списка анимаций
+        def MakeFrameAnim(animWay,delay):
+            FrameAnim=[]
+            for anim in animWay:
+                FrameAnim.append((anim, delay))
+            ReadyAnim = pyganim.PygAnimation(FrameAnim)
+            return ReadyAnim
+        
+        #Анимация покоя
+        self.PAStay=MakeFrameAnim(AnStay,AnimationDelay)
+        self.PAStay.play()
+
+        # Анимация  движения вправо
+        self.PARight=MakeFrameAnim(AnRight,AnimationDelay)
+        self.PARight.play()
+
+        # Анимация движения влево
+        self.PALeft=MakeFrameAnim(AnLeft,AnimationDelay)
+        self.PALeft.play()
 
     # Функция обновления слайма при нажатии клавиш
     def update(self,left,right,jump,platforms):
         if left:
             self.moveX=-MoveSpeed
+            self.image.fill((0,0,0))
+            self.PALeft.blit(self.image,(0,0))
         if right:
             self.moveX=+MoveSpeed
+            self.image.fill((0,0,0))
+            self.PARight.blit(self.image,(0,0))
         if not(left or right):
             self.moveX=0
+            if not jump:
+                self.image.fill((0,0,0))
+                self.PAStay.blit(self.image,(0,0))
         if jump:
             if self.onGround:
                 self.moveY=-jumpPower
